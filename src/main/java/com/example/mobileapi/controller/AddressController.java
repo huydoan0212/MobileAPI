70bfe86e74cf79f4d026733d7ebbf60b8655ec4a
huydoan0212 <21130374@st.hcmuaf.edu.vn>
2024-06-20 10:56:35 +0700

    initial

--- src/main/java/com/example/mobileapi/controller/AddressController.java
+++ src/main/java/com/example/mobileapi/controller/AddressController.java
@@ -3,7 +3,6 @@ package com.example.mobileapi.controller;
 import com.example.mobileapi.service.AddressService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
