3bffc61b87fab762d54d8440e1c0fba8f1323318
PeterPann295 <21130354@st.hcmuaf.edu.vn>
2024-06-28 12:54:02 +0700

    OkeOKe

--- src/main/java/com/example/mobileapi/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/mobileapi/service/impl/CustomerServiceImpl.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
